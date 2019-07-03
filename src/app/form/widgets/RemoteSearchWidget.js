e56a19b03168c4988958dc4fec23fb4362268cd8
Alessandro Sebastiani <sebbalex@gmail.com>
2019-07-03 12:45:18 +0200

    Riuso - Codice IPA: - Query are now enforced and more specific on what user is searching for - Field style now looks cleaner

--- src/app/form/widgets/RemoteSearchWidget.js
+++ src/app/form/widgets/RemoteSearchWidget.js
@@ -1,247 +1,264 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import {Field} from "redux-form";
+import { Field } from "redux-form";
 import Info from "../../components/Info";
-import {Combobox} from "react-widgets";
+import { Combobox } from "react-widgets";
 import validator from "validator";
 
 class RSComponent extends Component {
-    _localProps;
+	_localProps;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            text: '',
-            initialValue: false,
-            reset: false,
-            items: []
-        };
-        this.convertProps(props);
-        this.onChange = this.onChange.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			text: '',
+			initialValue: false,
+			reset: false,
+			items: []
+		};
+		this.convertProps(props);
+		this.onChange = this.onChange.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    convertProps(props) {
-        //converting props array in obj
-        const data = props.children.find(v => {
-            return typeof v === 'object'
-        });
-        this._localProps = data;
-    }
+	convertProps(props) {
+		//converting props array in obj
+		const data = props.children.find(v => {
+			return typeof v === 'object'
+		});
+		this._localProps = data;
+	}
 
-    query(value) {
-        value = typeof value === 'object' ?
-            value.ipa : value;
-        const callParams = this._localProps.schema.ajax;
-        const myHeaders = new Headers();
-        myHeaders.append('Content-Type', 'application/json');
-        const query = callParams.params(value);
+	query(value) {
+		value = typeof value === 'object' ?
+			value.ipa : value;
+		const callParams = this._localProps.schema.ajax;
+		const myHeaders = new Headers();
+		myHeaders.append('Content-Type', 'application/json');
+		const query = callParams.params(value);
 
-        const myInit = {
-            method: 'POST',
-            headers: myHeaders,
-            mode: 'cors',
-            cache: 'default',
-            body: JSON.stringify(query)
-        };
+		const myInit = {
+			method: 'POST',
+			headers: myHeaders,
+			mode: 'cors',
+			cache: 'default',
+			body: JSON.stringify(query)
+		};
 
-        if (!validator.isURL(callParams.url, {require_tld: false}))
-            return false;
+		if (!validator.isURL(callParams.url, { require_tld: false }))
+			return false;
 
-        const request = new Request(
-            callParams.url, myInit
-        );
+		const request = new Request(
+			callParams.url, myInit
+		);
 
-        fetch(request)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    if (!result.hits) throw Error('query malformed');
-                    this.setState({
-                        isLoaded: true,
-                        items: result.hits.hits.map(v => {
-                            return v._source
-                        })
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    console.error(error)
-                    this.setState({
-                        text: value,
-                        isLoaded: true,
-                        error
-                    });
-                }
-            );
-    }
+		fetch(request)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					if (!result.hits && Array.isArray(result.hits)) throw Error('query malformed');
+					this.setState({
+						isLoaded: true,
+						items: result
+					});
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					console.error(error)
+					this.setState({
+						text: value,
+						isLoaded: true,
+						error
+					});
+				}
+			);
+	}
 
-    handleChange(event) {
-        const val = event.target.value;
-        if (this._localProps.input.onChange) {
-            if (val == null) this._localProps.input.onChange("");
-            else this._localProps.input.onChange(val);
-        }
-        this.setState({text: val});
-    }
+	handleChange(event) {
+		const val = event.target.value;
+		console.log(val, event)
+		if (this._localProps.input.onChange) {
+			if (val == null) this._localProps.input.onChange("");
+			else this._localProps.input.onChange(val);
+		}
+		this.setState({ text: val });
+	}
 
-    onChange(val) {
-        if (val.length > 1)
-            this.query(val);
-        else
-            this.query('');
-        //return to editor
-        if (this._localProps.input.onChange) {
-            if (val == null) this._localProps.input.onChange("");
-            else this._localProps.input.onChange(val.ipa);
-        }
-        //setting initial and actual value
-        this.setState({text: val, initialValue: false});
-    }
+	onChange(val) {
+		if (val.length > 1)
+			this.query(val);
+		else
+			this.query('');
+		//return to editor
+		if (this._localProps.input.onChange) {
+			if (val == null) this._localProps.input.onChange("");
+			else this._localProps.input.onChange(val.ipa);
+		}
+		//setting initial and actual value
+		this.setState({ text: val, initialValue: false });
+	}
 
-    componentDidMount() {
-        this.setState({initialValue: true});
-        this.query(this._localProps.input.value);
-    }
+	componentDidMount() {
+		this.setState({ initialValue: true });
+		this.query(this._localProps.input.value);
+	}
 
-    /**
-     * Data modelling
-     * @param result data
-     * @param item
-     * @returns {{link: string, description: *, ipa: (Document.ipa|*), value: string, pec: string}}
-     */
-    modelData(result, item = {}) {
-        return {
-            ipa: result.ipa,
-            description: item.description || result.description
-        };
-    }
+	/**
+	 * Data modelling
+	 * @param result data
+	 * @param item
+	 * @returns {{link: string, description: *, ipa: (Document.ipa|*), value: string, pec: string}}
+	 */
+	modelData(result, item = {}) {
+		return {
+			ipa: result.ipa,
+			description: item.description || result.description,
+			pec: item.pec || result.pec
+		};
+	}
 
-    manipulateData(items) {
-        let res = [];
-        let t = this;
-        items
-            .map(function (result) {
-                let out = [];
-                if (result.office && Array.isArray(result.office))
-                    result.office.forEach(function (item) {
-                        out.push(t.modelData(result, item));
-                    });
-                out.push(t.modelData(result));
-                return out;
-            })
-            .forEach(function (r) {
-                r.forEach(function (result) {
-                    res.push(result)
-                });
-            });
-        return res;
-    }
+	manipulateData(items) {
+		let res = [];
+		let t = this;
+		items.hits.hits
+			.map((result) => {
+				let out = [];
+				let office = result.inner_hits.office.hits.hits;
+				let _source = result._source;
+				if (office && Array.isArray(office))
+					//if offices are searched and returned
+					if (office.length > 0)
+						office.forEach((item) => {
+							out.push(t.modelData(_source, item._source));
+						});
+					//else if parent ipa are searched
+					else
+						out.push(t.modelData(_source));
+				return out;
+			})
+			.forEach((r) => {
+				r.forEach((result) => {
+					res.push(result)
+				});
+			});
+		return res;
+	}
 
-    render() {
-        const {error, isLoaded, items, text, initialValue} = this.state;
-        let ListItem = ({item}) => (
-            <span>
-                {item.description + " "}
-                (<strong>{item.ipa}</strong>)
-            </span>
-        );
+	getItem(items) {
+		if (Array.isArray(items.hits.hits) && items.hits.hits.length > 0) {
+			const item = items.hits.hits[0];
+			return item._source;
+		}
+		return '';
+	}
+
+	render() {
+		const { error, isLoaded, items, text, initialValue } = this.state;
+		let ListItem = ({ item }) => (
+			<span>
+				{item.description + " "}
+				<br />
+				<strong>ipa: </strong>{item.ipa}
+				<strong> pec: </strong>{item.pec}
+			</span>
+		);
 
 
-        if ((error) || (!isLoaded)) {
-            //fallback if network toward elastic has problems
-            const field = this._localProps;
-            return (
-                <input
-                    value={text}
-                    onChange={this.handleChange}
-                    required={field.required}
-                    className="form-control"
-                    placeholder={field.placeholder}
-                    maxLength={field.maxLength}
-                    minLength={field.minLength}
-                    disabled={field.schema.disabled}
-                />
-            );
-        } else {
-            return (
-                <Combobox
-                    value={initialValue ? items[0] : text}
-                    onChange={this.onChange}
-                    textField={item => typeof item === 'string' ? item : item.description + ' (' + item.ipa + ')'}
-                    itemComponent={ListItem}
-                    data={this.manipulateData(items)}
-                />
-            );
-        }
-    }
+		if ((error) || (!isLoaded)) {
+			//fallback if network toward elastic has problems
+			const field = this._localProps;
+			return (
+				<input
+					value={text}
+					onChange={this.handleChange}
+					required={field.required}
+					className="form-control"
+					placeholder={field.placeholder}
+					maxLength={field.maxLength}
+					minLength={field.minLength}
+					disabled={field.schema.disabled}
+				/>
+			);
+		} else {
+			return (
+				<Combobox
+					value={initialValue ? this.getItem(items) : text}
+					onChange={this.onChange}
+					textField={item => typeof item === 'string' ?
+						item : item.description + ' (' + item.ipa + ')'}
+					itemComponent={ListItem}
+					data={this.manipulateData(items)}
+				/>
+			);
+		}
+	}
 }
 
 
 const renderInput = field => {
-    const className = classNames([
-        "form-group", "riuso-box",
-        {"has-error": field.meta.touched && field.meta.error}
-    ]);
-    return (
-        <div className={className}>
-            <label className="control-label" htmlFor={"field-" + field.name}>
-                {field.label} {field.required ? "*" : ""}
-            </label>
+	const className = classNames([
+		"form-group", "riuso-box",
+		{ "has-error": field.meta.touched && field.meta.error }
+	]);
+	return (
+		<div className={className}>
+			<label className="control-label" htmlFor={"field-" + field.name}>
+				{field.label} {field.required ? "*" : ""}
+			</label>
 
-            <RSComponent>
-                {...field}
-                id={"field-" + field.fieldName}
-                required={field.required}
-                placeholder={field.placeholder}
-                maxLength={field.maxLength}
-                minLength={field.minLength}
-                disabled={field.disabled}
+			<RSComponent>
+				{...field}
+				id={"field-" + field.fieldName}
+				required={field.required}
+				placeholder={field.placeholder}
+				maxLength={field.maxLength}
+				minLength={field.minLength}
+				disabled={field.disabled}
 
-            </RSComponent>
+			</RSComponent>
 
-            {field.meta.touched &&
-            field.meta.error && (
-                <span className="help-block">{field.meta.error}</span>
-            )}
-            {field.description && (
-                <Info
-                    title={field.label ? field.label : field.name}
-                    description={field.description}
-                />
-            )}
-        </div>
-    );
+			{field.meta.touched &&
+				field.meta.error && (
+					<span className="help-block">{field.meta.error}</span>
+				)}
+			{field.description && (
+				<Info
+					title={field.label ? field.label : field.name}
+					description={field.description}
+				/>
+			)}
+		</div>
+	);
 };
 
 const RemoteSearchWidget = props => {
-    return (
-        <Field
-            component={renderInput}
-            label={props.label}
-            name={props.fieldName}
-            required={props.required}
-            disabled={props.disabled}
-            id={"field-" + props.fieldName}
-            placeholder={props.schema.default}
-            description={props.schema.description}
-            {...props}
-        />
-    );
+	return (
+		<Field
+			component={renderInput}
+			label={props.label}
+			name={props.fieldName}
+			required={props.required}
+			disabled={props.disabled}
+			id={"field-" + props.fieldName}
+			placeholder={props.schema.default}
+			description={props.schema.description}
+			{...props}
+		/>
+	);
 };
 
 RemoteSearchWidget.propTypes = {
-    schema: PropTypes.object.isRequired,
-    fieldName: PropTypes.string,
-    label: PropTypes.string,
-    theme: PropTypes.object,
-    multiple: PropTypes.bool,
-    required: PropTypes.bool
+	schema: PropTypes.object.isRequired,
+	fieldName: PropTypes.string,
+	label: PropTypes.string,
+	theme: PropTypes.object,
+	multiple: PropTypes.bool,
+	required: PropTypes.bool
 };
 
 
