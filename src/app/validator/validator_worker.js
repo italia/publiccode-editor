a512474cca152e20425587491d0910aedd0c4612
Alessandro Sebastiani <sebbalex@gmail.com>
2021-03-01 17:16:03 +0100

    fix: importScript mimetype mistake

--- src/app/validator/validator_worker.js
+++ src/app/validator/validator_worker.js
@@ -1,9 +1,9 @@
 import { validatorWasm } from "./validator_wasm";
+// eslint-disable-next-line no-undef
+importScripts("../../../validator-wasm/wasm_exec.js");
 
 onmessage = async (e) => {
   try {
-    // eslint-disable-next-line no-undef
-    importScripts("../../../validator-wasm/wasm_exec.js");
     validatorWasm(e.data, (validator) => {
       postMessage({validator});
     });
