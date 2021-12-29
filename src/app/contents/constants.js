2732970fb1b8ff9c0becfe463004a3e8f49c5d3c
Fabio Bonelli <fb@fabiobonelli.it>
2021-12-29 16:18:43 +0100

    fix: privacy policy link is a 404

--- src/app/contents/constants.js
+++ src/app/contents/constants.js
@@ -1,6 +1,6 @@
 let {REPOSITORY, ELASTIC_URL, VALIDATOR_URL, VALIDATOR_REMOTE_URL} = process.env;
 
-export const privacyPolicyUrl = `https://developers.italia.it/it/privacy-policy/`;
+export const privacyPolicyUrl = `https://developers.italia.it/it/privacy-policy`;
 export const repositoryUrl = `https://docs.italia.it/italia/developers-italia/publiccodeyml/it/master/`;
 export const versionsUrl = `https://api.github.com/repos/${REPOSITORY}/contents/version`;
 export const sampleUrl = `https://raw.githubusercontent.com/italia/pc-web-validator/master/tests/valid.minimal.yml`;
