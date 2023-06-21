5a04b8f03b6e82888151044f6105433a430c68c3
Nicola Squartini <tensor5@gmail.com>
2023-06-22 01:51:59 +0900

    chore: convert i18n module to TypeScript

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,27 +4,21 @@ import { DEFAULT_LANGUAGE } from "../app/contents/constants";
 import translation from "./translations.json";
 import pc from "./publiccode.json";
 
-const getTranslations = () => {
-  //checking keys
-  if (!Object.keys(translation).every((x) => Object.keys(pc).includes(x))) {
-    return translation;
-  }
-  const result = Object.keys(translation)
-    .map((x) => ({
-      [x]: { ...translation[x], ...pc[x] },
-    }))
-    .reduce((acc, x) => ({ ...acc, ...x }), {});
-  return result;
-};
-
 i18n.use(initReactI18next).init({
   ns: ["translation", "pc"],
   defaultNS: "translation",
-  resources: getTranslations(),
+  resources: {
+    en: {
+      pc: pc.en.pc,
+      translation: translation.en.translation,
+    },
+    it: {
+      pc: pc.it.pc,
+      translation: translation.it.translation,
+    },
+  },
   lng: DEFAULT_LANGUAGE,
   interpolation: {
     escapeValue: false,
   },
 });
-
-export default i18n;
