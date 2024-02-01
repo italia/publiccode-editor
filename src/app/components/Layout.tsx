7ee2f11bbab69382d6a2b736165d468f1ebd9239
Fabio Bonelli <fb@fabiobonelli.it>
2024-02-01 12:38:47 +0100

    fix: notification zindex and reset notification text (#309)

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,6 +1,7 @@
 import "../../asset/style.scss";
 import { NotificationManager } from "design-react-kit";
-import { NOTIFICATION_TIMEOUT } from "../contents/constants";
+
+const NOTIFICATION_TIMEOUT = 4_000;
 
 interface Props {
   children: React.ReactNode;
@@ -9,7 +10,7 @@ interface Props {
 
 const Layout = ({ children, isLoading }: Props): JSX.Element => (
   <div className={isLoading ? "wrapper loadingRemote" : "wrapper"}>
-    <NotificationManager duration={NOTIFICATION_TIMEOUT} fix="top" />
+    <NotificationManager duration={NOTIFICATION_TIMEOUT} fix="top" closeOnClick={false} style={{zIndex: 10}} />
     {children}
   </div>
 );
