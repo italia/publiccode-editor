3c2ef192eaf0e851ed6f5d9adf50aef1886013ce
Nicola Squartini <tensor5@gmail.com>
2024-03-04 12:21:46 +0100

    fix: make description field a textarea

--- src/app/components/Editor.tsx
+++ src/app/components/Editor.tsx
@@ -90,6 +90,7 @@ export default function Editor() {
                     fieldName={`description.${lang}.longDescription`}
                     lang={lang}
                     required
+                    textarea
                   />
                 </div>
               ))}
