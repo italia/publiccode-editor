f740caa7aa1aa18fc9dd460774ca417344c6d51a
valerio como <valeriocomo@gmail.com>
2024-09-27 13:17:03 +0000

    chore: removed copy in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite';
 import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react';
-import copy from 'rollup-plugin-copy';
 import favicons from '@peterek/vite-plugin-favicons'
 
 // https://vitejs.dev/config/
-export default defineConfig(()=> {
+export default defineConfig(() => {
   return {
     plugins: [
       react(),
-      favicons('public/assets/img/favicon-32x32.png'),
-      copy({
-        targets: [
-          { src: 'src/generated/main.wasm', dest: 'dist' },
-          { src: 'src/generated/wasm_exec.js', dest: 'dist' },
-        ],
-        hook: 'writeBundle',
-      }),
+      favicons('public/assets/img/favicon-32x32.png')
     ],
     build: { target: 'esnext' }
   } satisfies UserConfig;
