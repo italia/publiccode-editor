e9c714217756634d32df574dc332f8286da3a75f
valerio como <v.como@innovazione.gov.it>
2024-12-03 17:29:37 +0000

    chore: f

--- src/app/yaml-upload.spec.ts
+++ src/app/yaml-upload.spec.ts
@@ -1,30 +1,32 @@
 import { isYamlFile } from "./yaml-upload";
 
-describe('yaml upload helper functions test', () => {
+describe('yaml upload helper functions tests', () => {
 
-    const testFactory = (type: string, assertCondition: boolean, filename = 'publiccode.yml') => {
-        //arrange
-        const file = new File([""], filename, { type });
-        //act
-        const actual = isYamlFile(file);
-        //assert
-        expect(actual).toBe(assertCondition)
-    }
+    describe('isYamlFile function tests', () => {
 
-    it('should return true if mime type is application/yaml (Chrome)', () => {
-        testFactory('application/yaml', true);
-    })
+        const testFactory = (type: string, assertCondition: boolean, filename = 'publiccode.yml') => {
+            //arrange
+            const file = new File([""], filename, { type });
+            //act
+            const actual = isYamlFile(file);
+            //assert
+            expect(actual).toBe(assertCondition)
+        }
 
-    it('should return true if mime type is application/x-yaml (Firefox)', () => {
-        testFactory('application/x-yaml', true);
-    })
+        it('should return true if mime type is application/yaml (Chrome)', () => {
+            testFactory('application/yaml', true);
+        })
 
-    it('should return true if mime type is text/yaml', () => {
-        testFactory('text/yaml', true);
-    })
+        it('should return true if mime type is application/x-yaml (Firefox)', () => {
+            testFactory('application/x-yaml', true);
+        })
 
+        it('should return true if mime type is text/yaml', () => {
+            testFactory('text/yaml', true);
+        })
 
-    it('should return false if mime type is application/json', () => {
-        testFactory('application/json', false);
+        it('should return false if mime type is application/json', () => {
+            testFactory('application/json', false);
+        })
     })
 })
