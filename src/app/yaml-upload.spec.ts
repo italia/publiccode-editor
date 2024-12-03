d575ca1d1da71538e42d65ae344970cfc9eea3ec
valerio como <v.como@innovazione.gov.it>
2024-12-03 17:25:46 +0000

    chore: test

---
+++ src/app/yaml-upload.spec.ts
@@ -0,0 +1,30 @@
+import { isYamlFile } from "./yaml-upload";
+
+describe('yaml upload helper functions test', () => {
+
+    const testFactory = (type: string, assertCondition: boolean, filename = 'publiccode.yml') => {
+        //arrange
+        const file = new File([""], filename, { type });
+        //act
+        const actual = isYamlFile(file);
+        //assert
+        expect(actual).toBe(assertCondition)
+    }
+
+    it('should return true if mime type is application/yaml (Chrome)', () => {
+        testFactory('application/yaml', true);
+    })
+
+    it('should return true if mime type is application/x-yaml (Firefox)', () => {
+        testFactory('application/x-yaml', true);
+    })
+
+    it('should return true if mime type is text/yaml', () => {
+        testFactory('text/yaml', true);
+    })
+
+
+    it('should return false if mime type is application/json', () => {
+        testFactory('application/json', false);
+    })
+})
