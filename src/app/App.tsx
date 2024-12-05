8e873f9c1f6d453a65a10262c4d8bd33b93334f3
Lorezz <point.point@gmail.com>
2024-12-05 17:29:02 +0100

    chore : empty space

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import { useTranslation } from 'react-i18next'
-import store from './store'
+import { useState } from "react";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+import store from "./store";
 import { Provider } from "react-redux";
-import Editor from './components/Editor'
-import Layout from './components/Layout'
-
+import Editor from "./components/Editor";
+import Layout from "./components/Layout";
 
 export const App = () => {
   const [isLoading] = useState(false);
   const { t } = useTranslation();
-
   return (
     <Provider store={store}>
       {isLoading && (
-        <div className="d-flex align-items-center col-12 position-absolute h-100 w-100">
-          <div className="mr-auto ml-auto">
+        <div className='d-flex align-items-center col-12 position-absolute h-100 w-100'>
+          <div className='mr-auto ml-auto'>
             <h3>{t("validation.inprogress")}</h3>
             <div
-              className="spinner-grow text-primary"
-              role="status"
-              aria-hidden="true"
+              className='spinner-grow text-primary'
+              role='status'
+              aria-hidden='true'
             ></div>
           </div>
         </div>
@@ -32,4 +30,4 @@ export const App = () => {
   );
 };
 
-export default App
+export default App;
