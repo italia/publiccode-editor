fd4962ff8302f7ab74c4a19ccf07fb55dac94b2e
Valerio Como <v.como@innovazione.gov.it>
2025-01-20 08:08:22 +0000

    feat: added release version in console

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app/App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './app/App.tsx';
 // import './index.css'
+import packageJson from '../package.json';
+
+console.log(`${packageJson.name} ${packageJson.version}`);
 
 createRoot(document.getElementById('app')!).render(
   // <StrictMode>
-    <App />
+  <App />
   // </StrictMode>,
 )
