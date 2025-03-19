32fe2664aa6c68b1b04d9362da7e9a50bc79f65d
Alessandro Palazzesi <alessandro.palazzesi@gmail.com>
2025-03-19 15:00:50 +0100

    fix: yaml / yml upload error on Windows

--- src/app/yaml-upload.ts
+++ src/app/yaml-upload.ts
@@ -4,7 +4,19 @@ const yamlMimeTypes = [
     'text/yaml'
 ]
 
-export const isYamlFile = (file?: File | null) => Boolean(file?.type && yamlMimeTypes.includes(file?.type.toLowerCase()))
+export const isYamlFile = (file?: File | null) => {
+    if (!file) return false;
+
+    if (file.type && yamlMimeTypes.includes(file.type.toLowerCase())) {
+        return true;
+    }
+
+    if (file.type === "" && navigator.userAgent.includes("Windows")) {
+        return file.name.endsWith(".yml") || file.name.endsWith(".yaml");
+    }
+
+    return false;
+};
 
 export const hasYamlFileExtension = (value?: string) => {
     const ext = value?.split(/[. ]+/).pop();
